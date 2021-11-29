75550023e9b1aede7de6a1c0a6ec2e419d4796ea
Korawit Prutsachainimmit <korawit.p@phuket.psu.ac.th>
2021-11-29 16:55:16 +0700

    Add landmark detection

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 async function quickstart() {
-    // Imports the Google Cloud client library
-    const vision = require('@google-cloud/vision');
-  
-    // Creates a client
-    const client = new vision.ImageAnnotatorClient();
-  
-    // Performs label detection on the image file
-    const [result] = await client.labelDetection('./resources/Bq-fjx7gCOO.jpg');
-    const labels = result.labelAnnotations;
-    console.log('Labels:');
-    labels.forEach(label => console.log(label.description));
-  }
-  quickstart();
+  // Imports the Google Cloud client library
+  const vision = require('@google-cloud/vision');
+
+  // Creates a client
+  const client = new vision.ImageAnnotatorClient();
+
+  const fileName = './resources/BrII286n9s-.jpg';
+
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection(fileName);
+  const labels = result.labelAnnotations;
+  console.log('Labels:');
+  labels.forEach(label => {
+    console.log(label.description)
+  });
+
+  // Performs landmark detection on the image file
+  const [result2] = await client.landmarkDetection(fileName);
+  const landmarks = result2.landmarkAnnotations;
+  console.log('Landmarks:');
+  landmarks.forEach(landmark => console.log(landmark));
+}
+quickstart();
